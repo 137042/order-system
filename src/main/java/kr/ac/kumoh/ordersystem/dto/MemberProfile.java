1219ba7de19783353f1126b63d4ed205a73dee13
NemoDouble <ehdud1228@naver.com>
2023-06-10 15:06:12 +0900

    Update Oauth login to fill customer roletype as customer

--- src/main/java/kr/ac/kumoh/ordersystem/dto/MemberProfile.java
+++ src/main/java/kr/ac/kumoh/ordersystem/dto/MemberProfile.java
@@ -1,6 +1,7 @@
 package kr.ac.kumoh.ordersystem.dto;
 
 import kr.ac.kumoh.ordersystem.domain.Member;
+import kr.ac.kumoh.ordersystem.domain.MemberRoleType;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -17,6 +18,7 @@ public class MemberProfile {
                 .name(name)
                 .email(email)
                 .provider(provider)
+                .role(MemberRoleType.CUSTOMER)
                 .build();
     }
 }
